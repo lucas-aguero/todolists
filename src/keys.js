8ce3ad459bf862851a34f0267a70f269a9eae721
lucas-aguero <lucasaguero@gmail.com>
2022-02-28 19:50:09 -0300

    Remote Mysql Created

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,15 @@
 module.exports = {
+  // database: {
+  //   host: "localhost",
+  //   user: "lucasaguero",
+  //   password: "g1308modeon",
+  //   database: "todolists",
+  // },
+
   database: {
-    host: "localhost",
-    user: "lucasaguero",
-    password: "g1308modeon",
-    database: "todolists",
+    host: "remotemysql.com",
+    user: "wW1IsIgkxh",
+    password: "WkIj6gwfBo",
+    database: "wW1IsIgkxh",
   },
 };
